ceb7b52ad68d0249c06d7d36f70a90c4984c3ec0
anjalisoni5547 <anjalisoni5547@gmail.com>
2026-07-29 12:56:03 +0530

    Add basic array input and output in firstday.cpp
    array

---
+++ firstday.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{
+    //cout<<"Hello World";
+ int arr[3]={};
+     for(int i=0; i<3; i++){
+        cin>>arr[i];
+        cout<<arr[i]<<" "; 
+        
+     }
+     
+  return 0;
+}
